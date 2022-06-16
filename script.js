2f98a9562d58a4524a811e5d7a573d1d2888ace5
Kenan Kadic <ken.kadic@outlook.com>
2022-06-16 14:10:59 -0500

    update to scope

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+// PRACTICING THE SCOPE CHAIN
+
+//SCOPE is entire region where a variable is accessible.
+
+function calcAge(birthYear){
+    const age = 2037 - birthYear;
+
+    // javaScript does a variable look up--finds firstName in global scope and can use it
+    function printAge() {
+        const output = `You are ${age}, born in ${birthYear}`
+        console.log(output);
+    }
+    printAge()
+
+    return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
